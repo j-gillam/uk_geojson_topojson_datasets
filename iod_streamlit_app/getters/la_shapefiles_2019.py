15f13ad436d82a1338cfc96dad875d31d6ac9f3f
j-gillam <107113232+j-gillam@users.noreply.github.com>
2023-01-24 17:29:25 +0000

    Delete iod_streamlit_app directory

--- iod_streamlit_app/getters/la_shapefiles_2019.py
+++
@@ -1,9 +0,0 @@
-import altair as alt
-
-def get_la_shapefiles_2019()->alt.Data:
-
-    geojson_la = "https://raw.githubusercontent.com/j-gillam/uk_geojson_topojson_datasets/main/la_clean_shapefiles_2019.geojson"
-    geodata_la = alt.Data(
-                url=geojson_la, format=alt.DataFormat(property="features", type="json")
-            )
-    return geodata_la
